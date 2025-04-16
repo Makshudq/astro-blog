04f35dd34d7236eadc85fe63814ca326a809936b
makshud <makshudq@gmail.com>
2025-04-16 11:46:27 +0530

    astro app basic code

---
+++ src/data/navData.js
@@ -0,0 +1,14 @@
+export const navData = [
+  // {
+  //   name: "Home",
+  //   path: "/",
+  // },
+  {
+    name: "About",
+    path: "/", 
+  },
+  {
+    name: "Blogs",
+    path: "/blog",
+  },
+];
